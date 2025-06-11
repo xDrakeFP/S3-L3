b530c046b85b5473804b7b4819ab8c851c1145ad
xDrakeFP <fpini3q@gmail.com>
2025-06-11 15:37:00 +0200

    Full excercise commit

---
+++ assets/js/script.js
@@ -0,0 +1,26 @@
+const form = document.getElementById("todolist");
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const taskElement = document.getElementById("taskform");
+  const task = taskElement.value;
+  const taskrow = document.createElement("div");
+  taskrow.innerHTML = `<li class="assignedtask">${task}</li> <button onclick="deleteTask(event)"><i class="fas fa-trash-alt"></i></button>`;
+  const divlist = document.getElementById("taskbox");
+  divlist.appendChild(taskrow);
+  form.reset();
+});
+const deleteTask = (e) => {
+  e.target.parentElement.remove();
+};
+
+const textlist = document.querySelector("ul");
+textlist.addEventListener(
+  "click",
+  (e) => {
+    if (e.target.tagName === "LI") {
+      console.log(e);
+      e.target.classList.toggle("barrato");
+    }
+  },
+  false
+);
